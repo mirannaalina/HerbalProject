aedcfd60021994bc2e6aae39c5ce8db858eaa2a1
mirannaalina <v_mxa@yahoo.com>
2019-12-16 15:50:04 +0200

    from nothing to something

---
+++ src/main/java/ro/herb/herbalproject/HerbalProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.herb.herbalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/herb/herbalproject/HerbalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.herb.herbalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
